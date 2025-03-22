330e196f8461210be8066dbc5649d21334547c60
huyngqa <huynguqa@gmail.com>
2025-03-22 17:49:46 +0700

    edit

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import LoginPage from '../apps/accounts/signin/LoginPage.vue';
 import SignUpPage from '../apps/accounts/signup/SignUpPage.vue';
 
@@ -9,7 +9,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 });
 
